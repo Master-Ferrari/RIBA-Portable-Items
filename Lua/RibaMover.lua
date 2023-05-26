39601c7377fa90da591e6e68342cb35cd1b3c4e0
Master-Ferrari <128731554+Master-Ferrari@users.noreply.github.com>
2023-05-26 18:06:39 +0300

    Depth changer fix

--- Lua/RibaMover.lua
+++ Lua/RibaMover.lua
@@ -21,11 +21,32 @@ if SERVER then
         end)
     end)
 
+    Hook.Add("loaded", "RIBADepth", function()
+        Networking.Receive("DepthMSG", function(msg, sender)
+            local Item = Entity.FindEntityByID(tonumber(msg.ReadString()))
+            local newSpriteDepth = msg.ReadInt16()
+            Item.SpriteDepth = newSpriteDepth
+        end)
+    end)
+
     return
 end
 
 -- GetPositionUpdateInterval
 
+
+RIBA.DepthAttached = function(value, Item)
+    if true then
+        local newSpriteDepth = math.round(value/1000.0, 3)
+        if (not Game.IsSingleplayer) then
+            local netMsg = Networking.Start("DepthMSG");
+            netMsg.WriteString(tostring(Item.ID))
+            netMsg.WriteInt16(newSpriteDepth)
+            Networking.Send(netMsg)
+        end
+        FocusedItem.SpriteDepth = newSpriteDepth
+    end
+end
 
 RIBA.moveAttached = function(H, V, Item)
     -- local attachable = RIBA.GetAttributeValueFromItem(Item, "Holdable", "attachable")=="true"
@@ -92,30 +113,36 @@ RIBA.decoratorUI = function(sprite, depthInt)
     end
 
 
-    local menuContent = GUI.Frame(GUI.RectTransform(Vector2(0.15, 0.1), menu.RectTransform, GUI.Anchor.Center))
+    local menuContent = GUI.Frame(GUI.RectTransform(Vector2(0.18, 0.115), menu.RectTransform, GUI.Anchor.BottomCenter))
 
     -- local menuContent = GUI.Frame(GUI.RectTransform(Vector2(0.15, 0.1), menu.RectTransform, GUI.Anchor.BottomCenter), "", Color(255,255,255,255)) -- основное окно
-    menuContent.RectTransform.AbsoluteOffset = Point(0, 110)
+    -- menuContent.RectTransform.AbsoluteOffset = Point(0, 110)
+    menuContent.RectTransform.RelativeOffset = Vector2(0, 0.1)
     -- menuContent.Color = Color(112,150,124,255)
     -- menuContent.Color = Color(0,0,0,0)
     -- menuContent.HoverColor = Color(0,0,0,0)
 
-    local menuH = GUI.ListBox(GUI.RectTransform(Vector2(1, 1), menuContent.RectTransform, GUI.Anchor.BottomCenter), true) -- содержимое горизонталь
+    local menuH = GUI.ListBox(GUI.RectTransform(Vector2(1, 1), menuContent.RectTransform, GUI.Anchor.BottomCenter), true, Color(0,0,0,0), nil) -- содержимое горизонталь
 
-    local imageFrame = GUI.Frame(GUI.RectTransform(Point(93, 93), menuH.Content.RectTransform, GUI.Anchor.CenterLeft), "GUITextBox", Color(0,0,0,0))  ---иконка
-    local image = GUI.Image(GUI.RectTransform(Point(93, 93), imageFrame.RectTransform, GUI.Anchor.Center), sprite)
+    local imageFrame = GUI.Frame(GUI.RectTransform(Point(100, 100), menuH.Content.RectTransform, GUI.Anchor.CenterLeft), "GUITextBox", Color(0,0,0,0), "InnerFrame")  ---иконка
+    imageFrame.RectTransform.RelativeOffset = Vector2(0.062, 0)
+    local image = GUI.Image(GUI.RectTransform(Point(100, 100), imageFrame.RectTransform, GUI.Anchor.Center), sprite)
     imageFrame.CanBeFocused = false
-
-    local menuHV = GUI.ListBox(GUI.RectTransform(Vector2(0.665, 1), menuH.Content.RectTransform, GUI.Anchor.CenterLeft), false) -- содержимое вертикаль
+    
+    --правая часть
+    local menuHV = GUI.ListBox(GUI.RectTransform(Vector2(0.6, 0.9), menuH.Content.RectTransform, GUI.Anchor.CenterLeft), false, Color(0,0,0,150), "InnerFrame") -- содержимое вертикаль
     menuHV.Color = Color(0,0,0,0)
     menuHV.HoverColor = Color(0,0,0,0)
-
-    local menuHVH0 = GUI.ListBox(GUI.RectTransform(Vector2(1, 0.4), menuHV.Content.RectTransform, GUI.Anchor.TopCenter), true, Color(0,0,0,0)) -- содержимое вертикаль
+    menuHV.RectTransform.RelativeOffset = Vector2(0.058, 0)
+    
+    --Крутилки
+    local menuHVH0 = GUI.ListBox(GUI.RectTransform(Vector2(1, 0.33), menuHV.Content.RectTransform, GUI.Anchor.TopCenter), true, Color(0,0,0,0), nil) -- содержимое вертикаль
+    menuHVH0.RectTransform.RelativeOffset = Vector2(0.018, 0.06)
     -- menuHVH0.Color = Color(0,0,0,0)
     -- menuHVH0.HoverColor = Color(0,0,0,0)
     -- menuHVH0.OutlineColor = Color(0,0,0,0)
     menuHVH0.Padding = Vector4(0, 0, 0, 0)
-    menuHVH0.Spacing = 7
+    menuHVH0.Spacing = 3
     menuHVH0.PadBottom = false
     menuHVH0.CanBeFocused = false
 
@@ -127,10 +154,10 @@ RIBA.decoratorUI = function(sprite, depthInt)
         depthInput.IntValue = depthInt
     end
     depthInput.OnValueChanged = function ()
-        FocusedItem.SpriteDepth = math.round(depthInput.IntValue/1000.0, 3)
+        RIBA.DepthAttached(depthInput.IntValue, FocusedItem)
     end
 
-    local gradInput = GUI.NumberInput(GUI.RectTransform(Vector2(0.46, 0.5), menuHVH0.Content.RectTransform), NumberType.Int) -- крутилка
+    local gradInput = GUI.NumberInput(GUI.RectTransform(Vector2(0.46, 0.4), menuHVH0.Content.RectTransform), NumberType.Int) -- крутилка
     gradInput.MinValueInt = 001
     gradInput.MaxValueInt = 900
     gradInput.valueStep = 10
@@ -140,8 +167,9 @@ RIBA.decoratorUI = function(sprite, depthInt)
     gradInput.OnValueChanged = function ()
         FocusedItem.SpriteDepth = math.round(gradInput.IntValue/1000.0, 3)
     end
-    
-    local menuHVH1 = GUI.ListBox(GUI.RectTransform(Vector2(0.95, 0.25), menuHV.Content.RectTransform, GUI.Anchor.TopCenter), true, Color(0,0,0,0)) -- содержимое вертикаль
+
+    --Отражалки
+    local menuHVH1 = GUI.ListBox(GUI.RectTransform(Vector2(0.95, 0.2), menuHV.Content.RectTransform, GUI.Anchor.TopCenter), true, Color(0,0,0,0), nil) -- содержимое вертикаль
     menuHVH1.Color = Color(0,0,0,0)
     menuHVH1.HoverColor = Color(0,0,0,0)
     menuHVH1.Padding = Vector4(0, 0, 0, 0)
@@ -155,8 +183,9 @@ RIBA.decoratorUI = function(sprite, depthInt)
     FlipXButton.OnClicked = function ()
         RIBA.flipAttached(false, FocusedItem)
     end
-    
-    local menuHVH2 = GUI.ListBox(GUI.RectTransform(Vector2(0.95, 0.25), menuHV.Content.RectTransform, GUI.Anchor.TopCenter), true, Color(0,0,0,0)) -- содержимое вертикаль
+
+    --Большие кнопочки
+    local menuHVH2 = GUI.ListBox(GUI.RectTransform(Vector2(0.95, 0.2), menuHV.Content.RectTransform, GUI.Anchor.TopCenter), true, Color(0,0,0,0), nil) -- содержимое вертикаль
     menuHVH2.Color = Color(0,0,0,0)
     menuHVH2.HoverColor = Color(0,0,0,0)
     menuHVH2.Padding = Vector4(0, 0, 0, 0)
@@ -164,19 +193,43 @@ RIBA.decoratorUI = function(sprite, depthInt)
 
     local leftButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "L", GUI.Alignment.Center, "GUIButtonSmall")
     leftButton.OnClicked = function ()
-        RIBA.Move(-10,0,FocusedItem)
+        RIBA.moveAttached(-10,0,FocusedItem)
     end
     local rightButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "R", GUI.Alignment.Center, "GUIButtonSmall")
     rightButton.OnClicked = function ()
-        RIBA.Move(10,0,FocusedItem)
+        RIBA.moveAttached(10,0,FocusedItem)
     end
     local upButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "U", GUI.Alignment.Center, "GUIButtonSmall")
     upButton.OnClicked = function ()
-        RIBA.Move(0,10,FocusedItem)
+        RIBA.moveAttached(0,10,FocusedItem)
     end
     local downButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "D", GUI.Alignment.Center, "GUIButtonSmall")
     downButton.OnClicked = function ()
-        RIBA.Move(0,-10,FocusedItem)
+        RIBA.moveAttached(0,-10,FocusedItem)
+    end
+    
+    --Маленькие кнопочки
+    local menuHVH2 = GUI.ListBox(GUI.RectTransform(Vector2(0.95, 0.2), menuHV.Content.RectTransform, GUI.Anchor.TopCenter), true, Color(0,0,0,0), nil) -- содержимое вертикаль
+    menuHVH2.Color = Color(0,0,0,0)
+    menuHVH2.HoverColor = Color(0,0,0,0)
+    menuHVH2.Padding = Vector4(0, 0, 0, 0)
+    menuHVH2.CanBeFocused = false
+
+    local leftButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "l", GUI.Alignment.Center, "GUIButtonSmall")
+    leftButton.OnClicked = function ()
+        RIBA.moveAttached(-1,0,FocusedItem)
+    end
+    local rightButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "r", GUI.Alignment.Center, "GUIButtonSmall")
+    rightButton.OnClicked = function ()
+        RIBA.moveAttached(1,0,FocusedItem)
+    end
+    local upButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "u", GUI.Alignment.Center, "GUIButtonSmall")
+    upButton.OnClicked = function ()
+        RIBA.moveAttached(0,1,FocusedItem)
+    end
+    local downButton = GUI.Button(GUI.RectTransform(Vector2(0.25, 1), menuHVH2.Content.RectTransform), "d", GUI.Alignment.Center, "GUIButtonSmall")
+    downButton.OnClicked = function ()
+        RIBA.moveAttached(0,-1,FocusedItem)
     end
 
     menu.Visible = true
